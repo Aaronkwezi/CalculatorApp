f98f1883d35fd9844c0896781a250b26274a37c3
Aaron Kwezi <isingomaron@gmail.com>
2026-09-02 21:20:30 -0500

    created mathematical functions and tested them

---
+++ javascript.js
@@ -0,0 +1,21 @@
+
+
+function add(a,b){
+    return a+b;
+}
+function subtract(a,b){
+    return console.log(a-b);
+}
+function multiply(a,b){
+    return console.log(a*b);
+}
+function divide(a,b){
+    return console.log(a/b);
+}
+const a = 36;
+const b = 6;
+
+console.log(add(a,b));
+console.log(subtract(a,b));
+console.log(multiply(a,b));
+console.log(divide(a,b));
