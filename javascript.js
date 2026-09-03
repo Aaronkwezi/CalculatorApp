e42723642a8814b46732b7fa307d44a3e98fb09e
Aaron Kwezi <isingomaron@gmail.com>
2026-09-02 22:52:29 -0500

    Added event listeners and variables for calculations

--- javascript.js
+++ javascript.js
@@ -4,18 +4,63 @@ function add(a,b){
     return a+b;
 }
 function subtract(a,b){
-    return console.log(a-b);
+    return a-b;
 }
 function multiply(a,b){
-    return console.log(a*b);
+    return a*b;
 }
 function divide(a,b){
-    return console.log(a/b);
+    return a/b;
 }
-const a = 36;
-const b = 6;
 
-console.log(add(a,b));
-console.log(subtract(a,b));
-console.log(multiply(a,b));
-console.log(divide(a,b));
+let fnumber;
+let selectedOperator;
+let Snumber;
+function operate(fnumber,Snumber,oper){
+
+    if (oper==="+"){
+        return add(fnumber,Snumber);
+    }
+    if(oper==="-"){
+        return subtract(fnumber,Snumber);
+    }
+    if(oper==="x"){
+        return multiply(fnumber,Snumber);
+    }
+    if(oper==="/"){
+        return divide(fnumber,Snumber);
+    }
+}
+
+const input = document.querySelector(".queries");
+input.textContent="";
+const numbers =  document.querySelectorAll(".btn");
+
+numbers.forEach(function(button){
+    button.addEventListener("click",()=>{
+        input.append(button.value);
+    });
+});
+
+const operators = document.querySelectorAll(".operator");
+operators.forEach(function(button){
+    button.addEventListener("click",()=>{
+        fnumber = parseFloat(input.textContent);
+        selectedOperator=button.value;
+        input.textContent="";
+        
+    });
+});
+
+const equals = document.querySelector(".eoperator");
+equals.addEventListener("click",()=>{
+    Snumber=parseFloat(input.textContent);
+    result = operate(fnumber,Snumber,selectedOperator);
+    input.textContent=result;
+})
+
+
+
+
+
+
